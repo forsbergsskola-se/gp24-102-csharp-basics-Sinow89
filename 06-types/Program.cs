97353c2ce3ffc9540e1d3a09d8449409598a8afa
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-16 11:56:50 +0200

    Finished program

--- 06-types/Program.cs
+++ 06-types/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string outputString = "Hello World";
+Console.WriteLine(outputString);
+float decimalValue = 0.3f;
+Console.WriteLine(decimalValue);
+bool isAwesome = true;
+Console.WriteLine(isAwesome);
