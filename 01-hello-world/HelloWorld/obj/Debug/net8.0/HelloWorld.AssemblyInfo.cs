a45953e60f489c3df6e51a4ad0eae1007c6816dd
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-16 09:25:12 +0200

    Test to build

--- 01-hello-world/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ 01-hello-world/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5f330c6d7e9bb97f35db240e39dcba4bb1cfa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6794fd54882e0d76d8406bb5f4c8eca47f968f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
