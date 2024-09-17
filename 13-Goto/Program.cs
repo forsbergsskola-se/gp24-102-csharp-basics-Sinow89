94bef4e1485b53317c416908e87e3fd8fe369c86
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2024-09-17 20:21:09 +0200

    Program done

--- 13-Goto/Program.cs
+++ 13-Goto/Program.cs
@@ -1,9 +1,14 @@
-﻿AskToBuy:
-Console.WriteLine("I have picked a number (1-100). It's your turn to guess it!");
+﻿Console.WriteLine("I have picked a number (1-100). It's your turn to guess it!");
+AskToBuy:
 string answer = Console.ReadLine();
 double intAnswer = Convert.ToDouble(answer);
-if (answer != "42")
-{
-    goto AskToBuy;
-}
+
+if (answer == "42") 
+    
+{ Console.WriteLine("That's the number! Well played!"); }
+
+else if (intAnswer <42)  { Console.WriteLine("Nope! My number is Greater!"); }
+else if (intAnswer >42)  { Console.WriteLine("Nope! My number is Smaller!"); }
+
+{ goto AskToBuy; }
 Console.WriteLine("Thanks for buying my game!");
