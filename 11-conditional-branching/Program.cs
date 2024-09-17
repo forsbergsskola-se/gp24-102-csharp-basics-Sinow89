48b9291d470bf39383ffb5cb79c5c7ceceaa17ad
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-17 10:03:49 +0200

    Update Program.cs

--- 11-conditional-branching/Program.cs
+++ 11-conditional-branching/Program.cs
@@ -11,4 +11,9 @@ bool isAdult = IntAge >= 20;
 if (isChild) {Console.WriteLine("You are a child.");}
 if (isTeenager) {Console.WriteLine("You are a teenager.");}
 if (isAdult)   {Console.WriteLine("You are an adult.");}
-
+Console.WriteLine("Give me another integer.");
+string input2 = Console.ReadLine();
+string Interger = input2;
+int IntInterger = Convert.ToInt32(Interger);
+bool isOdd = IntInterger < 30;
+if (isOdd) {Console.WriteLine("The maximum is :" + input2);}
