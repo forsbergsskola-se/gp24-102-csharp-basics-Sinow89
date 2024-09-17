2caa82e7d340b4d034b540f1a7cddb4f744965d6
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-17 10:44:10 +0200

    Output is right but not right code

--- 11-conditional-branching/Program.cs
+++ 11-conditional-branching/Program.cs
@@ -1,19 +1,27 @@
 ﻿
-
-
 Console.WriteLine("Whats your age?");
+//First input start and definitions
 string input = Console.ReadLine();
 string age = input;
 int IntAge = Convert.ToInt32(age);
 bool isChild = IntAge > 0 && IntAge < 12;
 bool isTeenager = IntAge > 13 && IntAge < 19;
 bool isAdult = IntAge >= 20;
+
+//first if behaviour
 if (isChild) {Console.WriteLine("You are a child.");}
 if (isTeenager) {Console.WriteLine("You are a teenager.");}
 if (isAdult)   {Console.WriteLine("You are an adult.");}
 Console.WriteLine("Give me another integer.");
+
+//Next input start and definitions
 string input2 = Console.ReadLine();
 string Interger = input2;
 int IntInterger = Convert.ToInt32(Interger);
 bool isOdd = IntInterger < 30;
-if (isOdd) {Console.WriteLine("The maximum is :" + input2);}
+int x = IntInterger;
+int y = 11;
+int sum = x + y;
+//second if behaviour
+if (isOdd) {Console.WriteLine("The maximum is :" +sum);}
+if (IntInterger>21) {Console.WriteLine("Thats an odd number.");} 
