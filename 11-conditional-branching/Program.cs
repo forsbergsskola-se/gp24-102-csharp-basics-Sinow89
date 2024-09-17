45b5316fc725dcbc1032934f1fcc283225a8ae01
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-17 09:41:52 +0200

    Started P11 Exercise

---
+++ 11-conditional-branching/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+Console.WriteLine("Whats your age?");
+string input = Console.ReadLine();
+string age = input;
+int IntAge = Convert.ToInt32(age);
+bool isChild = IntAge > 0 && IntAge < 12;
+bool isTeenager = IntAge > 13 && IntAge < 19;
+bool isAdult = IntAge >= 20;
+if (isChild) {Console.WriteLine("You are a child.");}
+if (isTeenager) {Console.WriteLine("You are a teenager.");}
+if (isAdult)   {Console.WriteLine("You are an adult.");}
+
