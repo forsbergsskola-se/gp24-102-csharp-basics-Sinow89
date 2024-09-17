01af94a8a6d81109ecdb6ad34a1f2967b8eeadc2
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2024-09-17 19:47:45 +0200

    Beta

--- 13-Goto/Program.cs
+++ 13-Goto/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿AskToBuy:
+Console.WriteLine("I have picked a number (1-100). It's your turn to guess it!");
+string answer = Console.ReadLine();
+double intAnswer = Convert.ToDouble(answer);
+if (answer != "42")
+{
+    goto AskToBuy;
+}
+Console.WriteLine("Thanks for buying my game!");
