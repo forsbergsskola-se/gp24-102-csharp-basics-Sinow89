247a4ed09f7b663ced5b823137ad9efd6e4c456b
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-18 10:06:34 +0200

    Update Program.cs
    Writing first part of the code

--- 09-operators/Program.cs
+++ 09-operators/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Output:Give me a number of seconds
+// Input:123456
+// Output:Seconds: 36
+// Output:Minutes: 17
+// Output:Hours: 10
+// Output:Days: 1
+// Output:1.10:17:36
+// Output:In total, that's 1,4288888888888887 Days.
+
+Console.WriteLine("Give me a number of seconds");
+Console.ReadLine();
