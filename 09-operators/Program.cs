25d9bdbd661c0c399367b5bc8bc606e5ec5249a3
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-18 10:58:17 +0200

    Update Program.cs
    80% done

--- 09-operators/Program.cs
+++ 09-operators/Program.cs
@@ -9,11 +9,18 @@
 
 Console.WriteLine("Give me a number of seconds");
 string? seconds = Console.ReadLine();
-float x = Convert.ToInt32(seconds);
-float y = x / 60; //How many minutes
-float z = y / 60; // How many hours
-float w = z / 24; //How many days
-Console.WriteLine(y);
-Console.WriteLine(z);
-Console.WriteLine(w);
+int x = Convert.ToInt32(seconds);
+// float y = x / 60; //How many minutes
+// float z = y / 60; // How many hours
+// float w = z / 24; //How many days
+int modulo = x % 60; // 2057,6
+int modulo2 = (x/60) % 60; // 34,29333
+int modulo3 = (x/3600) % 24; // 1,4288
+int modulo4 = (x/3600) / 24;
+
+Console.WriteLine("Seconds: " + modulo);
+Console.WriteLine("Minutes: " + modulo2);
+Console.WriteLine("Hours: " + modulo3);
+Console.WriteLine("Days: " + modulo4);
+
 
