823d212f5b08c23289f5f892581dc1fb9992c45d
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-23 10:21:28 +0200

    Update Program.cs

--- 14-NiM/Program.cs
+++ 14-NiM/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+Console.WriteLine("Welcome to Nim!");
+int matchNumber = 24;
+char x = '|';
+string numberOfMatches = string.Empty;
+for (int i = 0; i < 24; i++)
+{
+    numberOfMatches += x.ToString();
+}
+Console.WriteLine($"{numberOfMatches} ({matchNumber})");
+Console.WriteLine("How many matches do you want to draw?");
+Console.ReadLine();
