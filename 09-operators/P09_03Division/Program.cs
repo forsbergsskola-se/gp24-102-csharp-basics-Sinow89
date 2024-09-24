0288c2fe7ebc7c27f6a1a8870a2d0d27a4b2d47d
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-24 15:51:08 +0200

    New program

---
+++ 09-operators/P09_03Division/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Give me a two numbers.");
+int userInput;
+while (!int.TryParse(Console.ReadLine(), out userInput))
+{
+    Console.WriteLine("Wrong input");
+}
+
+double division = (userInput);
+division = Math.Floor(division*10);
+Console.WriteLine(division);
+
+    
