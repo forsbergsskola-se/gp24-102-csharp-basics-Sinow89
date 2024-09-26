39cc3b2ce8c2a5606250de97ffc3150bbc92e981
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-26 11:21:01 +0200

    P09_07Product

---
+++ 09-operators/P09_07Product/Program.cs
@@ -0,0 +1,26 @@
+﻿{
+    ThreadStart:
+    Console.WriteLine("Give me a two numbers.");
+    float userInput1, userInput2;
+
+    // First input loop
+    {
+        while (!float.TryParse(Console.ReadLine(), out userInput1))
+        {
+            Console.WriteLine("Wrong input");
+        }
+    }
+    // Second input loop
+    {
+        while (!float.TryParse(Console.ReadLine(), out userInput2))
+        {
+            Console.WriteLine("Wrong input");
+        }
+    }
+    //The division result of the two inputs
+    float multiplication = (userInput1 * userInput2);
+    Console.WriteLine(multiplication);
+    
+    goto ThreadStart;
+}
+    
