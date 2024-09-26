92791200e3c73ad40a76699715eb5820d3845c38
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-26 10:44:48 +0200

    Negation program
    This program doesn't use ternary it just multiplies the input with -1.
    Lazy code.

---
+++ 09-operators/P09_06Negation/Program.cs
@@ -0,0 +1,19 @@
+﻿{
+    ThreadStart:
+    Console.WriteLine("Give me a number.");
+    float userInput1, userInput2;
+
+    // First input loop
+    {
+        while (!float.TryParse(Console.ReadLine(), out userInput1))
+        {
+            Console.WriteLine("Wrong input");
+        }
+    }
+    
+    float negation  = (userInput1*-1);
+    Console.WriteLine(negation);
+    
+    goto ThreadStart;
+}
+    
---
+++ Test/Program.cs
@@ -0,0 +1,10 @@
+﻿{
+
+    int number = 25;
+
+// Using ternary operator with minus
+    int result = number > 20 ? number - 10 : number - 5;
+
+    Console.WriteLine(result);
+    
+}
