a1fbde13785e25d55a8592da19649dc6bf640a20
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-26 11:59:16 +0200

    Created a Hypotenuse calculator

---
+++ 09-operators/P09_10SecondsToMinutes/Program.cs
@@ -0,0 +1,28 @@
+﻿{
+    ThreadStart:
+    Console.WriteLine("Give me the length and height of the triangle");
+    double length, height;
+
+    // First input loop
+    {
+        while (!double.TryParse(Console.ReadLine(), out length))
+        {
+            Console.WriteLine("Wrong input");
+        }
+    }
+    // Second input loop
+    {
+        while (!double.TryParse(Console.ReadLine(), out height))
+        {
+            Console.WriteLine("Wrong input");
+        }
+    }
+    //The division result of the two inputs
+    
+    double x = ((length*length) + (height*height));
+    double hypotenuse = Math.Sqrt(x);
+    Console.WriteLine("The hypotenuse is :" + hypotenuse);
+    
+    goto ThreadStart;
+}
+    
